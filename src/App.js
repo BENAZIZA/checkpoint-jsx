f2276e397af27b47f7ed2d6cb16a9e076cc14203
BENAZIZA <benazizahassen01@gmail.com>
2020-05-16 05:49:51 +0100

    checkpoint jsx

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import email from './imageInSrc.PNG'
+import './style.css'
+function App() {
+  return (
+    <div className="App">
+      <div style={{border:"solid 1px black",maxWidth:"100vw"}}>
+            <h1 className="title red">Hassen BEN AZIZA</h1><br/>    
+    <img src={email} className="styleImage" /> <br/>
+    <img src ="/imageInPublic.PNG" className="styleImage" /> 
+    </div>
+    <video style={{width:"320", height:"240"}} controls>   
+        <source src="/myVideo.mp4" type="video/mp4" />
+    </video> 
+    </div>
+    
+  );
+}
+
+export default App;
